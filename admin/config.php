4391164604cfa10663b8567fd343d02a8c4e81cf
Max Usikov <max.usikov@gmail.com>
2018-12-03 22:37:13 +0200

    Add base sources

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://svetodiodi/admin/');
+define('HTTP_CATALOG', 'http://svetodiodi/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://svetodiodi/admin/');
+define('HTTPS_CATALOG', 'http://svetodiodi/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Job/Svetodiod/www/admin/');
+define('DIR_SYSTEM', 'D:/Job/Svetodiod/www/system/');
+define('DIR_IMAGE', 'D:/Job/Svetodiod/www/image/');
+define('DIR_LANGUAGE', 'D:/Job/Svetodiod/www/admin/language/');
+define('DIR_TEMPLATE', 'D:/Job/Svetodiod/www/admin/view/template/');
+define('DIR_CONFIG', 'D:/Job/Svetodiod/www/system/config/');
+define('DIR_CACHE', 'D:/Job/Svetodiod/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/Job/Svetodiod/www/system/storage/download/');
+define('DIR_LOGS', 'D:/Job/Svetodiod/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/Job/Svetodiod/www/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/Job/Svetodiod/www/system/storage/upload/');
+define('DIR_CATALOG', 'D:/Job/Svetodiod/www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'svetodiod');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://svetodiodi/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://svetodiodi/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Job/Svetodiod/www/catalog/');
+define('DIR_SYSTEM', 'D:/Job/Svetodiod/www/system/');
+define('DIR_IMAGE', 'D:/Job/Svetodiod/www/image/');
+define('DIR_LANGUAGE', 'D:/Job/Svetodiod/www/catalog/language/');
+define('DIR_TEMPLATE', 'D:/Job/Svetodiod/www/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/Job/Svetodiod/www/system/config/');
+define('DIR_CACHE', 'D:/Job/Svetodiod/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/Job/Svetodiod/www/system/storage/download/');
+define('DIR_LOGS', 'D:/Job/Svetodiod/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/Job/Svetodiod/www/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/Job/Svetodiod/www/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'svetodiod');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');

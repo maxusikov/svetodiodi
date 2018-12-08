ebf0216f0b3165f05b01624bd36015d34f402d28
Max Usikov <max.usikov@gmail.com>
2018-12-08 07:45:32 +0200

    Carousel timer fix

---
+++ admin/config_1.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://usikov.crazytest.studio/admin/');
+define('HTTP_CATALOG', 'http://usikov.crazytest.studio/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://usikov.crazytest.studio/admin/');
+define('HTTPS_CATALOG', 'http://usikov.crazytest.studio/');
+
+// DIR
+define('DIR_APPLICATION', '/home/o/office5c/usikov.crazytest.studio/public_html/admin/');
+define('DIR_SYSTEM', '/home/o/office5c/usikov.crazytest.studio/public_html/system/');
+define('DIR_IMAGE', '/home/o/office5c/usikov.crazytest.studio/public_html/image/');
+define('DIR_LANGUAGE', '/home/o/office5c/usikov.crazytest.studio/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/o/office5c/usikov.crazytest.studio/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/o/office5c/usikov.crazytest.studio/public_html/system/config/');
+define('DIR_CACHE', '/home/o/office5c/usikov.crazytest.studio/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/o/office5c/usikov.crazytest.studio/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/o/office5c/usikov.crazytest.studio/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/o/office5c/usikov.crazytest.studio/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/o/office5c/usikov.crazytest.studio/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home/o/office5c/usikov.crazytest.studio/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'office5c_usik');
+define('DB_PASSWORD', 'office5c_usik');
+define('DB_DATABASE', 'office5c_usik');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config_1.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://usikov.crazytest.studio/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://usikov.crazytest.studio/');
+
+// DIR
+define('DIR_APPLICATION', '/home/o/office5c/usikov.crazytest.studio/public_html/catalog/');
+define('DIR_SYSTEM', '/home/o/office5c/usikov.crazytest.studio/public_html/system/');
+define('DIR_IMAGE', '/home/o/office5c/usikov.crazytest.studio/public_html/image/');
+define('DIR_LANGUAGE', '/home/o/office5c/usikov.crazytest.studio/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/o/office5c/usikov.crazytest.studio/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/o/office5c/usikov.crazytest.studio/public_html/system/config/');
+define('DIR_CACHE', '/home/o/office5c/usikov.crazytest.studio/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/o/office5c/usikov.crazytest.studio/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/o/office5c/usikov.crazytest.studio/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/o/office5c/usikov.crazytest.studio/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/o/office5c/usikov.crazytest.studio/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'office5c_usik');
+define('DB_PASSWORD', 'office5c_usik');
+define('DB_DATABASE', 'office5c_usik');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
